091b5aed8299aa70c6fca5a1c617eb854601e845
Musili-Adebayo <adebayomusili@gmail.com>
2026-06-25 15:42:25 +0100

    Setup RBAC for snowflake Infrastruture with different role and users and warehouse

--- snowflake/production/provider.tf
+++ snowflake/production/provider.tf
@@ -11,11 +11,4 @@ terraform {
   }
 }
 
-provider "snowflake" {
-  organization_name = var.snowflake_org
-  account_name      = var.snowflake_account
-  user              = var.snowflake_user
-  password          = var.snowflake_password
-  role              = var.snowflake_role
-  warehouse         = "ATLANTIS_WH"
-}
+provider "snowflake" {}
