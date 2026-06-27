dbee3f798073d192f270a28c78d5f3ef1cdc5744
Musili-Adebayo <adebayomusili@gmail.com>
2026-06-27 23:58:24 +0100

    modified the snowflake infra

--- snowflake/production/data.tf
+++ snowflake/production/data.tf
@@ -1,12 +1,8 @@
-data "aws_ssm_parameter" "dbt_snowflake_password" {
-  name            = "/staging/elite/snowflake/snowflake_password"
+data "aws_ssm_parameter" "elite_dbt_snowflake_password" {
+  name = "/staging/elite/snowflake/snowflake_password"
   with_decryption = true
 }
 
 data "aws_iam_role" "elite_snowflake_s3_role" {
   name = "elite-snowflake-role"
-}
-
-data "aws_s3_bucket" "angel_city_health_data" {
-  bucket = "angel-city-health-data"
 }
--- snowflake/production/storage_integration.tf
+++ snowflake/production/storage_integration.tf
@@ -3,6 +3,6 @@ resource "snowflake_storage_integration_aws" "angel_city_s3_integration" {
   storage_provider          = "S3"
   enabled                   = true
   storage_aws_role_arn      = data.aws_iam_role.elite_snowflake_s3_role.arn
-  storage_allowed_locations = ["s3://${data.aws_s3_bucket.angel_city_health_data.bucket}/"]
+  storage_allowed_locations = "s3://angel-city-health-data/"
   comment                   = "Storage integration for Angel City data lake"
 }
