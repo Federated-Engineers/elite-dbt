55105eb2469443a1e335b78e151e9492159345c2
Musili-Adebayo <adebayomusili@gmail.com>
2026-06-28 00:01:30 +0100

    modified the snowflake infra

--- snowflake/production/storage_integration.tf
+++ snowflake/production/storage_integration.tf
@@ -3,6 +3,6 @@ resource "snowflake_storage_integration_aws" "angel_city_s3_integration" {
   storage_provider          = "S3"
   enabled                   = true
   storage_aws_role_arn      = data.aws_iam_role.elite_snowflake_s3_role.arn
-  storage_allowed_locations = "s3://angel-city-health-data/"
+  storage_allowed_locations = ["s3://angel-city-health-data/"]
   comment                   = "Storage integration for Angel City data lake"
 }
